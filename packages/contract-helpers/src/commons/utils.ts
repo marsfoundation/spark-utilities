848a2e5850e29cfb57e4b1996558e2e42abc2413
Sam MacPherson <xylom44@gmail.com>
2023-01-25 08:16:18 -0600

    change default token amounts

--- packages/contract-helpers/src/commons/utils.ts
+++ packages/contract-helpers/src/commons/utils.ts
@@ -96,7 +96,8 @@ export const mintAmountsPerToken: Record<string, string> = {
   AAVE: valueToWei('100', 18),
   BAT: valueToWei('100000', 18),
   BUSD: valueToWei('10000', 18),
-  DAI: valueToWei('10000', 18),
+  DAI: valueToWei('50000', 18),
+  SDAI: valueToWei('50000', 18),
   ENJ: valueToWei('100000', 18),
   KNC: valueToWei('10000', 18),
   LEND: valueToWei('1000', 18), // Not available on v2, but to support v1 faucet
@@ -110,7 +111,7 @@ export const mintAmountsPerToken: Record<string, string> = {
   SUSD: valueToWei('10000', 18),
   TUSD: '0', // The TusdMinter contract already mints the maximum
   UNI: valueToWei('1000', 18),
-  USDC: valueToWei('10000', 6),
+  USDC: valueToWei('50000', 6),
   USDT: valueToWei('10000', 6),
   WBTC: valueToWei('1', 8),
   YFI: valueToWei('1', 18),
